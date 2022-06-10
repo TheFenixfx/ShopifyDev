387aa1bfa3af117f9e152f6af4e37dd8eb946036
Jhon <knifefx@gmail.com>
2022-06-10 00:52:05 -0430

    tailwind ready

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './config/*.json',
+    './layout/*.liquid',
+    './assets/*.liquid',
+    './sections/*.liquid',
+    './snipets/*.liquid',
+    './templates/*.liquid',
+    './templates/*.json',
+    './templates/customers/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+ 
